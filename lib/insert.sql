6b56794d3bb0352eb6adc17cdefcb6185db57606
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-02 03:24:46 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,27 @@
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ()
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (
+  "Mr. Chocolate", 1, "male", "brown", "mean", 1
+);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (
+  "Mr. Chocolate", 4, "female", "brown", "mean", 1
+);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (
+  "Mr. Chocolate", 1, "male", "brown", "mean", 1
+);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (
+  "Mr. Chocolate", 1, "male", "brown", "mean", 1
+);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (
+  "Mr. Chocolate", 1, "male", "brown", "mean", 1
+);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (
+  "Mr. Chocolate", 1, "male", "brown", "mean", 1
+);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (
+  "Mr. Chocolate", 1, "male", "brown", "mean", 1
+);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (
+  "Mr. Chocolate", 1, "male", "brown", "mean", 1
+);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (
+  "Mr. Chocolate", 1, "male", "brown", "mean", 1
+);
