dc2c36f9189886bf0c5cbe0dffb991c532565d2f
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-02 03:26:07 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -14,14 +14,11 @@ INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (
   "Melissa", 1, "male", "brown", "mean", 1
 );
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (
-  "Mr. Chocolate", 1, "male", "brown", "mean", 1
+  "Grinch", 1, "male", "brown", "mean", 1
 );
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (
-  "Mr. Chocolate", 1, "male", "brown", "mean", 1
+  "Wendy", 1, "male", "brown", "mean", 1
 );
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (
-  "Mr. Chocolate", 1, "male", "brown", "mean", 1
-);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (
-  "Mr. Chocolate", 1, "male", "brown", "mean", 1
+  NULL, 1, "male", "brown", "mean", 1
 );
